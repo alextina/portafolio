ca918bbc7b4a444f4b31650a2da8ab55ce0a849f
alextina <castilloavilaa@gmail.com>
2023-05-29 13:40:42 -0500

    Implementando cambios de color: modo dark/light - Mejorando la paleta de colores - Implementando métodos en SharedServicesService - Reconociendo tema (modo) del sistema - Switcher del modo en el nav

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SharedServicesService } from '../services/shared-services.service';
 
 @Component({
@@ -6,13 +6,23 @@ import { SharedServicesService } from '../services/shared-services.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
-  constructor(
-    private sharedSvc: SharedServicesService,
-  ) { }
-
+  isDarkMode: boolean = false;
   showNav: boolean = false;
+
+  constructor(private sharedSvc: SharedServicesService) { }
+
+  ngOnInit(): void {
+    this.sharedSvc.darkMode$.subscribe(darkMode => {
+      this.isDarkMode = darkMode;
+    })
+  }
+
+  toggleDarkMode(): void {
+    this.isDarkMode = !this.isDarkMode;
+    this.sharedSvc.updateDarkMode(this.isDarkMode)
+  }
 
   goHome(): void {
     this.sharedSvc.goToHome();
--- src/app/services/shared-services.service.ts
+++ src/app/services/shared-services.service.ts
@@ -10,10 +10,34 @@ export class SharedServicesService {
   // para ocultar header y footer
   private showHeaderSubject = new BehaviorSubject<boolean>(true);
   private showFooterSubject = new BehaviorSubject<boolean>(true);
+  // Variable para controlar el estado del modo oscuro
+  private darkModeSubject = new BehaviorSubject<boolean>(true);
 
+  // para ocultar header y footer
   showHeader$ = this.showHeaderSubject.asObservable();
   showFooter$ = this.showFooterSubject.asObservable();
+  // Observable para escuchar cambios en el modo oscuro
+  darkMode$ = this.darkModeSubject.asObservable()
 
+  private readonly darkModeLocalStorageKey = 'darkMode';
+
+  constructor(private router: Router) {
+    const storedDarkMode = localStorage.getItem(this.darkModeLocalStorageKey);
+    if (storedDarkMode !== null) {
+      this.darkModeSubject.next(storedDarkMode === 'true');
+    } else {
+      // Obtener la configuración del color del sistema al cargar la página
+      const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+      this.updateDarkMode(prefersDarkMode);
+    }
+
+    // Escuchar los cambios en la configuración del color del sistema
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+      this.updateDarkMode(event.matches);
+    });
+  }
+
+  // para ocultar header y footer
   updateShowHeader(showHeader: boolean): void {
     this.showHeaderSubject.next(showHeader);
   }
@@ -22,11 +46,11 @@ export class SharedServicesService {
     this.showFooterSubject.next(showFooter);
   }
 
-
-  constructor(
-    private router: Router,
-
-  ) { }
+  // cambios en el modo oscuro
+  updateDarkMode(darkMode: boolean): void {
+    this.darkModeSubject.next(darkMode);
+    localStorage.setItem(this.darkModeLocalStorageKey, darkMode.toString());
+  }
 
   goToHome(): void {
     this.router.navigate(['']);
